c8eadfed3b4cb3f21a63d10d44ee7582f16b2c7a
Daniele <d.tarini@hyperborea.com>
2022-12-31 15:21:48 +0100

    Rachele pictures

--- birthday-ng/src/app/components/tanti-auguri/tanti-auguri.component.ts
+++ birthday-ng/src/app/components/tanti-auguri/tanti-auguri.component.ts
@@ -11,17 +11,10 @@ export class TantiAuguriComponent {
 
   @Input() letters: AuguriLetter[];
 
-  constructor(private sanitizer: DomSanitizer) {
-
-  }
-
   lettersClasses(): {[id: string]: boolean} {
     let result: {[id: string]: boolean} = {};
     this.letters.forEach(letter => result[`show-${letter.code}`] = letter.visible);
     return result;
   }
 
-  styleMe(letter: AuguriLetter) {
-    return this.sanitizer.bypassSecurityTrustStyle(`.show-${letter.code} .letter-${letter.code} {opacity: 1;}`);
-  }
 }
